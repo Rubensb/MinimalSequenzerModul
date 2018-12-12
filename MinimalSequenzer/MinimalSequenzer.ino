4f791277578cbc2986b9cb1721a5ea2e1ffe2a2c
RubensB <ru191999@gmail.com>
2018-12-12 21:36:55 +0100

    Change BPM processing

--- MinimalSequenzer/MinimalSequenzer.ino
+++ MinimalSequenzer/MinimalSequenzer.ino
@@ -2,15 +2,21 @@
  * Sequencer with adjustable offset
  * Rubens 2018
  */
-int button = 2;
-int poti = A0;
+
+
 int NUMBER_OF_LEDS = 8;
+
+int button = 2;
 int leds[8] = {4,5,6,7,8,9,10,11};
 int sequence[8] = {0,0,0,0,0,0,0,0};
-int offsets[8] = {0,0,0,0,0,0,0,0}; 
+int offsets[8] = {0,0,0,0,0,0,0,0};
+int cvs[8] = {0,0,0,0,0,0,0,0};
 unsigned long sustains[8] = {0,0,0,0,0,0,0,0};
-int bpm;
-int beat; 
+
+int bpmArray[3] = {0, 0, 0}
+int bpm = (bpmArray[0]*100)+(bpmArray[1]*10)+bpmArray[2];
+
+int beat;
 float interval;
 unsigned long timing = 0;
 unsigned long currentMillis;
@@ -19,18 +25,16 @@ void setup() {
   for (int i = 0; i < NUMBER_OF_LEDS; i++){
     pinMode(leds[i], OUTPUT);
   }
-  
+
   pinMode(button, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), setCurrentBeat, RISING);
-  setBpm(120);
   for (int i = 0; i < NUMBER_OF_LEDS; i++){
     setOffset(i, 0);
   }
 }
 
-void setBpm(int value){
-  bpm = value;
-  interval = 1000 / (bpm / 60);
+void setStepState(int step, int state) {
+  sequence[step] = state;
 }
 
 void setPattern(int id, int value) {
@@ -64,7 +68,7 @@ void loop() {
     if ( sustains[i] < millis())
       digitalWrite(leds[i], sequence[i]);
   }
-  
+
   if (currentMillis - timing >= offsets[(beat)%NUMBER_OF_LEDS]) {
     timing = currentMillis;
     setAndSustain((beat)%NUMBER_OF_LEDS, 50);
