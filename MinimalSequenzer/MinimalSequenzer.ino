80a630f2dc95e185a7cc927e151f8c6b34dddc9f
RubensB <ru191999@gmail.com>
2018-12-17 18:06:21 +0100

    Alles am worken

--- MinimalSequenzer/MinimalSequenzer.ino
+++ MinimalSequenzer/MinimalSequenzer.ino
@@ -1,51 +1,59 @@
 /*
- * MinimalSequencer with CV
+ * MinimalstepSequencer with CV
  * Rubens 2018
+ * I force myself to eat no Cheeseburger
  */
 
 #include <Button.h>
 
-//TODO: Assingn buttons
-Button btn (2, false, false, 100);
+Button btn0 (12, false, false, 100);
+Button btn1 (2, false, false, 100);
+Button btn2 (13, false, false, 100);
 
-//TODO: Sort variables and  rename them with sense!!
-int button = 2;
-int NUMBER_OF_STEPS = 8;
-int leds[8] = {4,5,6,7,8,9,10,11};
-int sequence[8] = {0,0,0,0,0,0,0,0};
+int const NUMBER_OF_STEPS = 8;
+int const clockIn = 0;
+int const clockOut = 0;
+int const triggerOut = 3;
+//int leds[8] = {4,5,6,7,8,9,10,11};
+int const leds[8] = {11,10,9,8,7,6,5,4};
+int stepSequence[8] = {0,0,0,0,0,0,0,0};
 int offsets[8] = {0,0,0,0,0,0,0,0};
 unsigned long sustains[8] = {0,0,0,0,0,0,0,0};
-int bpmArry[3] = {0, 0, 0};
+int bpmArry[3];
 int bpm;
 int beat;
 float interval;
-unsigned long timing = 0;
+unsigned long elapsedMillis = 0;
 unsigned long currentMillis;
-
-int stepSequence[NUMBER_OF_STEPS] = {0,0,0,0,0,0,0,0};
-int CVOuts[NUMBER_OF_STEPS] = {0,0,0,0,0,0,0,0};
+unsigned long tempMillis = 0;
+unsigned long duration = 50;
 
 void setup() {
   Serial.begin(9600);
-  setBpm(1,2,0);
+  setBpm(0, 1);
+  setBpm(1, 2);
+  setBpm(2, 0);
+  pinMode(triggerOut, OUTPUT);
   for (int i = 0; i < NUMBER_OF_STEPS; i++){
     pinMode(leds[i], OUTPUT);
     setOffset(i, 0);
   }
 }
 
-void setBpm(int n100, int n10, int n){
-  bpmArry[0] = n100;
-  bpmArry[1] = n10;
-  bpmArry[2] = n;
-  bpm = ((n100*100)+(n10*10)+n);
-  interval = 1000 / (bpm / 60);
+void setBpm(int pos, int value){
+  bpmArry[pos] = value;
+  bpm = ((bpmArry[0]*100)+(bpmArry[1]*10)+bpmArry[2]);
+  interval = (60000 / bpm);
   Serial.println(bpm);
   Serial.println(interval);
 }
 
 void setStepState(int step, int state) {
-  sequence[step] = state;
+  stepSequence[step] = state;
+}
+
+void invertStepState(int step) {
+  stepSequence[step] = stepSequence[step]? 0 : 1;
 }
 
 void clearSteps(){
@@ -58,14 +66,6 @@ void setOffset(int id, int value) {
   offsets[id] = value + interval;
 }
 
-void syncGates(int step) {
-
-}
-
-void syncCVs(int step) {
-
-}
-
 void setAndSustain(int id, int duration){
   digitalWrite(leds[id], HIGH);
   sustains[id] = millis() + duration;
@@ -73,24 +73,38 @@ void setAndSustain(int id, int duration){
 
 void loop() {
   currentMillis = millis();
+  tempMillis = millis();
+  btn0.read();
+  btn1.read();
+  btn2.read();
 
-  //TODO: Try without using this for loop
   for (int i = 0; i < NUMBER_OF_STEPS; i++){
     if ( sustains[i] < millis())
-      digitalWrite(leds[i], sequence[i]);
+      digitalWrite(leds[i], stepSequence[i]);
   }
 
-  if (currentMillis - timing >= offsets[(beat)%NUMBER_OF_STEPS]) {
-    timing = currentMillis;
+  if (currentMillis - elapsedMillis >= offsets[(beat)%NUMBER_OF_STEPS]) {
+    digitalWrite(triggerOut, HIGH);
     setAndSustain((beat)%NUMBER_OF_STEPS, 50);
+    elapsedMillis = currentMillis;
     beat++;
+  if (tempMillis - elapsedMillis >= interval + duration) {
+    digitalWrite(triggerOut, LOW);
+    tempMillis = currentMillis;
+    println("True");
+    }
   }
 
-  setStepState(2, 1);
-  setStepState(4, 1);
-  for (int i = 0; i < 8; i++) {
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(sustains[i]);
+  if (btn0.wasPressed()) {
+    invertStepState(0);
+    Serial.println("btn0");
+  }
+  if (btn1.wasPressed()) {
+    invertStepState(1);
+    Serial.println("btn1");
+  }
+  if (btn2.wasPressed()) {
+    invertStepState(2);
+    Serial.println("btn2");
   }
 }
--- testFile.ino
+++ testFile.ino
@@ -1,12 +1,30 @@
+int bpmArry[3] = {0, 0, 0};
+int bpm;
+int beat;
+float interval;
+unsigned long timing = 0;
+unsigned long currentMillis;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+
 }
 
 void loop() {
-  byte brightness;
-  if (Serial.availeble()) {
-    brightness = Serial.read();
-    analogWrite(2, brightness);
+  currentMillis = millis();
+  if (currentMillis - timing > millis() ){
+
   }
+
+}
+
+void setBpm(int n100, int n10, int n){
+  bpmArry[0] = n100;
+  bpmArry[1] = n10;
+  bpmArry[2] = n;
+  bpm = ((n100*100)+(n10*10)+n);
+  interval = 1000 / (bpm / 60);
+  Serial.println(bpm);
+  Serial.println(interval);
 }
