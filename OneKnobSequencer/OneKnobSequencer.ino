a9d1d86a25105e0f54e611bd44f5a18ee1d1f74e
RubensB <ru191999@gmail.com>
2019-01-16 22:59:34 +0100

    Es funktioniert

--- OneKnobSequencer/OneKnobSequencer.ino
+++ OneKnobSequencer/OneKnobSequencer.ino
@@ -8,61 +8,70 @@ int const led0 = A0;
 int const led1 = A3;
 
 int const btn = 2; //0
+int const swtch = 5;
 int const gate = 4; //1
 int const clockIn = 3;
 
 int stepSequence[16] = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
 int halfStep = 0;
-int fullStep = 0;
-int bar = 0;
+
+bool recording = false;
+bool clean = false;
 
 void setup() {
   pinMode(led0, OUTPUT);
   pinMode(led1, OUTPUT);
   pinMode(gate, OUTPUT);
+  pinMode(swtch, INPUT);
   attachInterrupt(digitalPinToInterrupt(clockIn), sync, RISING);
-  //attachInterrupt(digitalPinToInterrupt(btn), recordSequence, RISING);
+  attachInterrupt(digitalPinToInterrupt(btn), recordSequence, RISING);
   Serial.begin(9600);
+  ledState(0);
 }
 
 void loop() {
+  if (halfStep%8 == 0) {
+    ledState(2);
+  }
+  if ((digitalRead(swtch) == true) && (clean == false)) {
+    clearSequence();
+    clean = true;
+    recording = true;
+  }
+  if ((digitalRead(swtch) == true) && (clean == true)) {
+    recording = true;
+  }
+  if (digitalRead(swtch) == false) {
+    recording = false;
+    clean = false;
+  }
 }
 
 void recordSequence(){
-  int pos = ((halfStep%16)-1);
-  stepSequence[pos] = stepSequence[pos]? 0 : 1;
-  if (stepSequence[pos] == 1) {
-    digitalWrite(gate, LOW);
-    digitalWrite(gate, HIGH);
+  if (recording == true) {
     ledState(2);
+    int pos = ((halfStep%16)-1);
+    stepSequence[pos] = stepSequence[pos]? 0 : 1;
+    if (stepSequence[pos] == 1) {
+      digitalWrite(gate, LOW);
+      digitalWrite(gate, HIGH);
+    }
   }
 }
 
 void sync() {
   ++halfStep;
   digitalWrite(gate, LOW);
-  if ((halfStep%2) == 1) {
-    ++fullStep;
-    digitalWrite(gate, HIGH);
-  }
-  if ((halfStep%16) == 0) {
-    ++bar;
-  }
-  /*if (stepSequence[((halfStep%16)-1)] == 0) {
-    digitalWrite(gate, LOW);
+  if (stepSequence[((halfStep%16)-1)] == 1) {
     digitalWrite(gate, HIGH);
     ledState(1);
   }
   else if (stepSequence[((halfStep%16)-1)] == 0) {
     digitalWrite(gate, LOW);
     ledState(0);
-  }*/
-  Serial.print("Half fullStep: ");
+  }
+  Serial.print("Half Step: ");
   Serial.print(halfStep);
-  Serial.print(" | fullStep: ");
-  Serial.print(fullStep);
-  Serial.print(" | Bar: ");
-  Serial.println(bar);
 }
 
 void ledState(int state) {
@@ -80,5 +89,11 @@ void ledState(int state) {
   else if (state == 2) {
     analogWrite(led0, 255);
     analogWrite(led1, 0);
+  }
+}
+
+void clearSequence() {
+  for (int i = 0; i < 16; i++) {
+    stepSequence[i] = 0;
   }
 }
